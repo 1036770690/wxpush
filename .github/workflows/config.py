0800409acd396f128cde8d1eb95160dbcf64acb4
1036770690 <73639088+1036770690@users.noreply.github.com>
2022-08-22 20:32:45 +0800

    Add files via upload

---
+++ .github/workflows/config.py
@@ -0,0 +1,25 @@
+# 公众号配置
+# 公众号appId
+app_id = "wxf3c11e80d3021a6e"
+# 公众号appSecret
+app_secret = "8fee84044f34981986cc1eb4f9324e43"
+# 模板消息id
+template_id = "7LdOnNh-jC-4c6sr5JRM8WMlbe1VaMk8yyB2duFTvJ0"
+# 接收公众号消息的微信号
+user = "oV-nW6P3vBrkmxLFlZcRbvSclRkA"
+# user = "oV-nW6J50gkEBpVkNYEJBsFAKasY"
+
+# 信息配置
+# 所在省份
+province = "浙江"
+# 所在城市
+city = "台州"
+province2 = "山东"
+# 所在城市
+city2 = "济南"
+# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+birthday = "2000-4-11"
+birthday2 = "1996-10-14"
+# 在一起的日子，格式同上
+love_date = "2022-3-21"
+
